b0373ea1b87396c4ae9367a45f641a3556ff661e
email <tat-kazakovceva@mail.ru>
2022-07-31 21:07:13 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let disc = b**2 - 4 * a * c;
+  if (disс < 0) { 
+    arr = [];
+     } else if ( disс == 0) {
+      arr = (-b + Math.sqrt(discrim)) / (2*a);
+     } else if (disc > 0) {
+        arr = [(-b + Math.sqrt(discrim)) / (2*a), (-b - Math.sqrt(discrim)) / (2*a)];  
+ }
   return arr; // array
 }
 
