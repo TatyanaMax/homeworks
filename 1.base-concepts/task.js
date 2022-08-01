fb49b9bd919949e1b6b045a3be92b188dfe7ade9
email <tat-kazakovceva@mail.ru>
2022-08-01 19:48:41 +0300

    устранение ошибки в названии переменной

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
+  let arr;
   let disc = b**2 - 4 * a * c;
   if (disс < 0) { 
     arr = [];
      } else if ( disс == 0) {
-      arr = (-b + Math.sqrt(discrim)) / (2*a);
+      arr = (-b + Math.sqrt(disc)) / (2*a);
      } else if (disc > 0) {
-        arr = [(-b + Math.sqrt(discrim)) / (2*a), (-b - Math.sqrt(discrim)) / (2*a)];  
+        arr = [(-b + Math.sqrt(disc)) / (2*a), (-b - Math.sqrt(disc)) / (2*a)];  
  }
   return arr; // array
 }
